17ca3d8919f721cd4e0724fd02beb51539de14d5
ted <ted19berenger@gmail.com>
2019-06-22 14:43:47 +0200

    test connection of database

--- fbapp/__init__.py
+++ fbapp/__init__.py
@@ -1,2 +1,13 @@
+import os
 from flask import Flask
+
 from .views import app
+from . import models
+
+# models sqlalchemy to app
+models.db.init_app(app)
+
+
+@app.cli.command()
+def init_db():
+    models.init_db()
